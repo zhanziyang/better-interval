8f9a3fff982befdd6fd281c59787be1a045bdd1e
zhanziyang <zhanziyang126@hotmail.com>
2016-10-12 23:28:42 +0800

    1th done

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # loopRequest
-A piece of code for you to replace setInterval with requestAnimationFrame in order to create smoother and less buggy loop.
+With loopRequest, you can easily make repetitive function executions like you usually do with <code>setInterval</code>. Under the hood it uses <code>requestAnimationFrame</code> to achieve the same purpose, but with better performance.
+
+## Let's not use setInterval anymore!
+Why?
+- setInterval keeps running event when the browser tab or window is not active.
+- When the callback function takes longer to finish than the interval you set, it will not wait for the function thus resulting in enqueuing of multiple callback functions.
+
+See more: [Better Performance With requestAnimationFrame](https://dev.opera.com/articles/better-performance-with-requestanimationframe/) by Luz Caballero
+
+# Usage
+## new LoopRequest(callback, interval [, args])
+### Argumemts:
+* **callback**: <em>Function</em> | Repetitively executing function
+* **interval**: <em>Number(milliseconds) | **optional** </em>| Time gap between each loop. By default it matches the display refresh rate of browser, which usually is 60 fps. So it's default value is approximately 16.7 (≈1000/60)
+* **args**: <em>Array | **optional** </em>| Array of arguments to be passed into the callback function.
+
+### Methods:
+* .start()
+* .stop()
+
+## Example
+```javascript
+var loop;
+var left = 0;
+var func = function(arg1, arg2) {
+  var elem = document.querySelector("#time");
+  elem.innerHTML = new Date();
+  left += 1;
+  if (left >= 1000) {
+    loop.stop(); //stop looping!
+  }
+  elem.style.position = "relative";
+  elem.style.left = left + "px";
+  console.log(arg1 + ", " + arg2);
+};
+
+loop = new LoopRequest(func, 100, ["hello", "world"]);
+loop.start(); //start looping!
+```
---
+++ loopRequest.js
@@ -0,0 +1,88 @@
+(function () {
+  //rAF polyfill
+  var lastTime = 0;
+  var vendors = ['webkit', 'moz'];
+  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||    // Webkit中此取消方法的名字变了
+      window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function (callback) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
+      var id = window.setTimeout(function () {
+        callback(currTime + timeToCall);
+      }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  }
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function (id) {
+      clearTimeout(id);
+    };
+  }
+
+  Array.isArray = function (arg) {
+    return Object.prototype.toString.call(arg) === '[object Array]';
+  };
+} ());
+
+(function (window) {
+  /*
+    var loop = new Loop(action, interval, args);
+    Args: 
+      @action: function 循环执行函数
+      @interval: number 毫秒间隔 可选 默认1000/60（≈16.7。是根据浏览器的刷新率——大多数为60fps得出）
+      @args: array 传给action的参数构成的数组 可选
+    loop.start(); 开始循环
+    loop.stop(); 停止循环
+    取代setInterval做重复执行。
+    ⚠ 如果是用于动画，interval的值应省略或趋近1/60以达到流畅的效果，在回调函数中通过调整每帧的改变量来调整速度。
+  */
+  var LoopRequest = function (action, interval, args) {
+    if (typeof action != "function") {
+      throw new Error("First argument should be of type function");
+    }
+    if (interval && Array.isArray(interval)) {
+      args = interval;
+      interval = 0;
+    }
+    if (interval && isNaN(interval)) {
+      throw new Error("Second argument should be a number");
+    }
+    this.animationReq;
+    this.action = action;
+    this.interval = interval;
+    this.args = args || [];
+  };
+
+  LoopRequest.prototype.start = function () {
+    var now, lastFrameTime = 0, elapsed, lastReqTime = 0, reqGap;
+    var loopFunc = function (timestamp) {
+      reqGap = timestamp - lastReqTime;
+      lastReqTime = timestamp;
+      this.animationReq = requestAnimationFrame(loopFunc);
+      if (!this.interval || this.interval < reqGap) {
+        this.action.apply(this, this.args);
+      } else {
+        now = new Date();
+        elapsed = now - lastFrameTime;
+        if (elapsed > this.interval) {
+          lastFrameTime = now;
+          this.action.apply(this, this.args);
+        }
+      }
+    }.bind(this);
+
+    loopFunc();
+  };
+
+  LoopRequest.prototype.stop = function () {
+    cancelAnimationFrame(this.animationReq);
+  };
+
+  window.LoopRequest = LoopRequest;
+} (window));
